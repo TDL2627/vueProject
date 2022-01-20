fe6a7c6d8c7182e97d351b45747269c068a47c05
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-20 11:21:03 +0200

    added a component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="sex1">
+   <h1>To-Do List</h1>
+  <ul>
+    <li>
+      <to-do-item label="My ToDo Item 3"></to-do-item>
+    </li>
+  </ul>
+  </div>
+ 
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ToDoItem
   }
 }
 </script>
@@ -22,5 +33,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#sex1{
+  background-color: red;
+  color: white;
 }
 </style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,25 @@
+<template>
+     <div id="todo">
+    <input type="checkbox" id="todo-item" />
+    <label for="todo-item">My Todo Item1</label>
+    <input type="checkbox" id="todo-item1" />
+    <label for="todo-item1">My Todo Item2</label>
+    <input type="checkbox" id="todo-item2" />
+    <label for="todo-item2">{{label}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+     props: {
+      label: { required: true, type: String },
+      done: { default: false, type: Boolean }
+    }
+};
+</script>
+<style scoped>
+
+#todo{
+    background-color: green;
+}
+</style>
